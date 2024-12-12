5339dff47dae0037e51eb8254a9d6d04ecd73ead
andyzhou <andyzhou23@outlook.com>
2024-12-12 16:38:38 -0500

    jwt

--- chat_server/src/main.rs
+++ chat_server/src/main.rs
@@ -1,5 +1,6 @@
 use actix::Actor;
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web_httpauth::middleware::HttpAuthentication;
 use clap;
 mod db;
 use db::db_init;
@@ -10,6 +11,8 @@ use user::{get_history, login, signup};
 mod group;
 use group::{create_group, join_group, leave_group, list_groups};
 
+mod jwt;
+use jwt::http_validator;
 mod chat {
     pub mod chat_server;
     pub mod connection_actor;
@@ -24,10 +27,21 @@ async fn index() -> impl Responder {
     HttpResponse::Ok().body("Welcome to the Rust-powered chat server!")
 }
 
+async fn api_default() -> impl Responder {
+    HttpResponse::Ok().json(serde_json::json!({
+        "error": "Token is valid, but route not found, check the url."
+    }))
+}
+
 async fn not_found() -> impl Responder {
     HttpResponse::NotFound().json(serde_json::json!({
         "error": "Route not found, check the url."
     }))
+}
+
+#[derive(Clone)]
+struct AppConfig {
+    jwt_secret: String,
 }
 
 #[actix_web::main]
@@ -49,21 +63,29 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Failed to initialize database");
     let chat_server = ChatServer::new(pool.clone()).start();
-
+    let jwt_secret = "123456".to_string();
+    // let jwt_secret = generate_secret(); // disabled in development
+    let app_config = AppConfig { jwt_secret };
     println!("The server is currently listening on localhost:8080.");
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
             .app_data(web::Data::new(chat_server.clone()))
+            .app_data(web::Data::new(app_config.clone()))
             .service(index)
             .service(signup)
             .service(login)
-            .service(get_history)
-            .service(create_group)
-            .service(list_groups)
-            .service(join_group)
-            .service(leave_group)
-            .service(ws_connect)
+            .service(
+                web::scope("/api")
+                    .wrap(HttpAuthentication::bearer(http_validator))
+                    .service(get_history)
+                    .service(create_group)
+                    .service(list_groups)
+                    .service(join_group)
+                    .service(leave_group)
+                    .service(ws_connect)
+                    .default_service(web::route().to(api_default)),
+            )
             .default_service(web::route().to(not_found))
     })
     .bind("0.0.0.0:8080")?
---
+++ todo.md
@@ -0,0 +1,31 @@
+# TODO
+
+## Client
+
+### To learn
+
+- database: sqlite, pool, query
+- websocket: actix_web_actors::ws, streamHandler, Actor
+- jwt: jsonwebtoken (do at last)
+
+### To implement
+
+- user
+  - signup: username+password -> id or error
+  - login: username+password -> token or error
+  - logout: mainly on GUI
+
+- group
+  - create: group_name -> group_info(id, name, code)
+  - join: group_code -> group_info(id, name, code)
+  - leave: group_id -> ()
+  - list: user_id -> [group_info(id, name, code, members)] // get group infos for display
+
+- message
+  - send: send message
+  - receive: receive message and display
+  - history: (group_id, latest_msg_id) -> [message(id, group_id, sender_id, content, created_at)] // get message history for display
+
+## Server
+
+curl -H "Authorization: Bearer YOUR_ACCESS_TOKEN" http://localhost:8080/api/get_history
