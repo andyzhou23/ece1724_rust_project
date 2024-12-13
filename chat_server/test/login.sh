f50499341094ff6a06dcfa559ab7033861a450b0
andyzhou <andyzhou23@outlook.com>
2024-12-13 01:00:03 -0500

    remove server from client branch

--- chat_server/test/login.sh
+++
@@ -1,6 +0,0 @@
-for i in {1..20}; do
-    curl -w "\n" "http://localhost:8080/signup" --json "{\"username\":\"user_$i\", \"password\":\"123456\"}"
-done
-for i in {1..20}; do
-    curl -w "\n" "http://localhost:8080/login" --json "{\"username\":\"user_$i\", \"password\":\"123456\"}"
-done
--- chat_server/test/setup.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Kill any existing server process
-pkill -f "../target/debug/chat_server"
-sleep 1
-rm -f server.db
-cargo run
